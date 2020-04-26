05eb37789435021f729859c2597a25ef2af32f60
oya163 <oyeshsin@hotmail.com>
2020-04-25 20:54:47 -0400

    Finalized web app

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 from flask import Flask, render_template, request
 from nepali_stemmer.stemmer import NepStemmer
-app = Flask(__name__)             # create an app instance
-
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('post.html')
-
+    return render_template('index.html')
 
 @app.route('/post', methods=['POST'])
 def post():
+    errors = []
     value = request.form['input']
     nepstem = NepStemmer()
-    return nepstem.stem(value)
-    
+    results = nepstem.stem(value)
+    return render_template('index.html', errors=errors, results=results)
 
 
-
-
-if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+if __name__ == "__main__":
+    app.run()
